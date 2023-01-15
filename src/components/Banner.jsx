cc9bc54a084bd611aad504e16a22007c7205c6f4
Kevin Vink <kap.vink@student.han.nl>
2023-01-15 20:54:13 +0100

    Testing

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -8,7 +8,7 @@ function Banner() {
   };
 
   return (
-    <div className="w-full h-[50vh] 3xl:h-[30vh] grid grid-cols-4 xl:grid-cols-5 3xl:grid-cols-4 relative bg-banner bg-cover shadow-xl border-b-2 border-sky-400">
+    <div className="w-full h-[50vh] 3xl:h-[30vh] grid grid-cols-4 2xl:grid-cols-5 3xl:grid-cols-4 relative bg-banner bg-cover shadow-xl border-b-2 border-sky-400">
       <div className="col-span-4 lg:col-span-4 xl:col-span-3 z-20 py-14 px-6 xl:px-10 xl:py-10">
         <div className="w-full h-full flex flex-col items-center xl:items-start justify-between">
           <h1 className="text-3xl xl:text-6xl text-white font-black">Discover the Road of my Career!</h1>
@@ -47,7 +47,7 @@ function Banner() {
           </button>
         </div>
       </div>
-      <div className="z-20 hidden xl:block xl:col-span-2 3xl:col-span-1 relative">
+      <div className="z-20 hidden xl:block 2xl:col-span-2 3xl:col-span-1 relative">
         <img src="./standing.png" className="absolute bottom-0 right-12 xl:h-[61vh]  3xl:h-[37vh]" alt="me standing" />
       </div>
       <div className="w-full h-full bg-accent-500/80 top-0 left-0 absolute z-10" />
