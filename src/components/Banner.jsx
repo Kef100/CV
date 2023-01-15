0dc8739d1d9af3843127b6a1a188a7c5381fa679
Kevin Vink <kap.vink@student.han.nl>
2023-01-15 21:00:13 +0100

    Testing

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -8,8 +8,8 @@ function Banner() {
   };
 
   return (
-    <div className="w-full h-[50vh] 3xl:h-[30vh] grid grid-cols-4 3xl:grid-cols-4 relative bg-banner bg-cover shadow-xl border-b-2 border-sky-400">
-      <div className="col-span-4 lg:col-span-4 xl:col-span-2 z-20 py-14 px-6 xl:px-10 xl:py-10">
+    <div className="w-full h-[55vh] 3xl:h-[30vh] grid grid-cols-4 2xl:grid-cols-5 3xl:grid-cols-4 relative bg-banner bg-cover shadow-xl border-b-2 border-sky-400">
+      <div className="col-span-4 lg:col-span-4 xl:col-span-2 2xl:col-span-3 3xl:col-span-2 z-20 py-14 px-6 xl:px-10 xl:py-10">
         <div className="w-full h-full flex flex-col items-center xl:items-start justify-between">
           <h1 className="text-3xl xl:text-6xl text-white font-black">Discover the Road of my Career!</h1>
           <div className="text-white flex gap-x-1 text-sm xl:text-lg items-center">
@@ -48,7 +48,7 @@ function Banner() {
         </div>
       </div>
       <div className="z-20 hidden xl:block xl:col-span-2 3xl:col-span-1 relative">
-        <img src="./standing.png" className="absolute bottom-0 right-12 xl:h-[61vh] 3xl:h-[37vh]" alt="me standing" />
+        <img src="./standing.png" className="absolute bottom-0 right-12 xl:h-[67vh] 3xl:h-[37vh]" alt="me standing" />
       </div>
       <div className="w-full h-full bg-accent-500/80 top-0 left-0 absolute z-10" />
     </div>
