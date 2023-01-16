a3ab7e33d9f5f89b4f4d557de25ac356cf8306d8
Kevin <kefthegamer100@gmail.com>
2023-01-16 16:22:27 +0100

    15 rem

--- src/components/Preferences.jsx
+++ src/components/Preferences.jsx
@@ -20,7 +20,7 @@ function Preferences() {
       <h2 className="text-white font-bold text-2xl pb-4">Preferences</h2>
       <div className="grid xl:grid-cols-3 gap-16">
         {data.preferences.map((preference) => (
-          <div key={preference.name} className="w-full h-full bg-accent-300 px-6 py-4 flex flex-col gap-y-4 relative min-h-[20rem] lg:min-h-0">
+          <div key={preference.name} className="w-full h-full bg-accent-300 px-6 py-4 flex flex-col gap-y-4 relative min-h-[15rem] lg:min-h-0">
             <h3 className="text-white font-bold pt-4">{preference.name}</h3>
             <p className="absolute bg-sky-400/75 w-fit text-white p-2 rounded-full -top-4 right-4 shadow-lg">{chooseIcon(preference.icon)}</p>
             <p className="text-neutral-400">{preference.description}</p>
