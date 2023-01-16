fbaa224255270ef270f82e0d41a8cfd06ad698fb
Kevin <kefthegamer100@gmail.com>
2023-01-16 15:43:40 +0100

    lint height macos

--- src/components/Lint.jsx
+++ src/components/Lint.jsx
@@ -17,7 +17,7 @@ function Lint() {
 
   return (
     (!localStorageShowLint || showLint) && (
-    <div className="fixed z-50 text-white top-0 left-0 w-full h-full lg:h-fit py-4 bg-sky-500 grid lg:grid-cols-3">
+    <div className="fixed z-50 text-white top-0 left-0 w-full h-full lg:h-20 2xl:h-16 py-4 bg-sky-500 grid lg:grid-cols-3">
       <div className="justify-center items-center inline-flex gap-2 px-10 lg:px-0 col-start-2 flex-col lg:flex-row">
         <AlertTriangle size={25} />
         This page is still under development,
